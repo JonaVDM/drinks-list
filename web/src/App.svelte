3eb8667e0578e496fc71ffa3c429f4b02e0c00af
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-06 16:44:03 +0200

    add tailwind and daisy

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -3,6 +3,8 @@
 </script>
 
 <main>
+  <h1 class="text-2xl font-thin">Dingetje</h1>
+
   {#await PB.collection("drinks").getFullList() then drinks}
     <pre>{JSON.stringify(drinks, null, 2)}</pre>
   {/await}
