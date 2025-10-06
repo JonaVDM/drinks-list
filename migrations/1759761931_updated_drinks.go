f1ee58f1f21ab82db623abf52aed6c4cf58e6d14
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-06 19:03:14 +0200

    add barcode

---
+++ migrations/1759761931_updated_drinks.go
@@ -0,0 +1,63 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3720670791")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": [
+				"CREATE INDEX ` + "`" + `idx_jtLmg8Uk5E` + "`" + ` ON ` + "`" + `drinks` + "`" + ` (` + "`" + `barcode` + "`" + `)"
+			]
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// add field
+		if err := collection.Fields.AddMarshaledJSONAt(4, []byte(`{
+			"autogeneratePattern": "",
+			"hidden": false,
+			"id": "text2544763494",
+			"max": 0,
+			"min": 0,
+			"name": "barcode",
+			"pattern": "",
+			"presentable": false,
+			"primaryKey": false,
+			"required": true,
+			"system": false,
+			"type": "text"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3720670791")
+		if err != nil {
+			return err
+		}
+
+		// update collection data
+		if err := json.Unmarshal([]byte(`{
+			"indexes": []
+		}`), &collection); err != nil {
+			return err
+		}
+
+		// remove field
+		collection.Fields.RemoveById("text2544763494")
+
+		return app.Save(collection)
+	})
+}
