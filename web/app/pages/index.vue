5ea39c0694a3c536af1ed9a1add39248681b5ff5
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-09 13:59:04 +0200

    add layout

---
+++ web/app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const pb = usePocketbase()
+
+const { data } = await useAsyncData(() => {
+  return pb.collection('drinks').getFullList()
+})
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold text-center">Hello World</h1>
+
+    <pre>{{ JSON.stringify(data, null, 2) }}</pre>
+  </div>
+</template>
