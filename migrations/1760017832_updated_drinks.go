23f1081001873cdff5523ad0c2d6d592a66f023d
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-09 17:03:34 +0200

    add simple order view

---
+++ migrations/1760017832_updated_drinks.go
@@ -0,0 +1,56 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3720670791")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "number4024315073",
+			"max": null,
+			"min": 0,
+			"name": "price",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3720670791")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"hidden": false,
+			"id": "number4024315073",
+			"max": null,
+			"min": 0,
+			"name": "prijs",
+			"onlyInt": false,
+			"presentable": false,
+			"required": false,
+			"system": false,
+			"type": "number"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
--- web/app/pages/(auth)/profile.vue
+++ web/app/pages/(auth)/profile.vue
@@ -1,11 +1,49 @@
 <script setup lang="ts">
+import type { RecordModel } from 'pocketbase'
+
 definePageMeta({
   middleware: 'auth'
 })
 
 const userStore = useUserStore()
+const pb = usePocketbase()
+
+const { data, error } = await useAsyncData(() => {
+  return pb.collection('orders').getFullList({ filter: `user.id='${userStore.user?.id}'`, expand: 'drink', sort: '-created' })
+})
+
+const ordersByMonth = computed(() => data.value?.reduce<Record<string, RecordModel[]>>((prev, curr) => {
+  const [year, month] = curr.created.split('-');
+  const key = `${year}-${month}`;
+  if (!prev[key]) {
+    prev[key] = [];
+  }
+  prev[key].push(curr)
+  return prev
+}, {}))
 </script>
 
 <template>
-  <pre>{{ JSON.stringify(userStore.user, null, 2) }}</pre>
+  <div v-if="error" class="alert alert-error">Something went wrong fetching data</div>
+  <div v-for="(orders, month) in ordersByMonth" class="overflow-x-auto">
+    <h2 class="text-xl">{{ month }}</h2>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Drink</th>
+          <th>Cost</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="order in orders">
+          <td>{{ new Date(order.created).toLocaleString() }}</td>
+          <td>{{ order.expand?.drink.drink }}</td>
+          <td>{{ order.expand?.drink.price }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
