73b359c84490f59325ec5c76bdee04b01e3054b7
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-09 15:02:58 +0200

    add logout button

--- web/app/layouts/default.vue
+++ web/app/layouts/default.vue
@@ -1,4 +1,18 @@
+<script setup lang="ts">
+const userStore = useUserStore()
+const pb = usePocketbase()
+
+function logout() {
+  pb.authStore.clear()
+  reloadNuxtApp()
+}
+</script>
+
 <template>
+  <div v-if="userStore.authed" class="p-2 text-right">
+    <p>Loged in as {{ userStore.user?.name }}</p>
+    <button class="btn btn-sm btn-error" @click="logout">Logout</button>
+  </div>
   <div class="max-w-6xl mx-auto">
     <slot />
   </div>
