334e2fc69cfa538ea11f2e82c6597eddfcbb5ca2
Jonathan van der Meulen <jonajvdm@gmail.com>
2025-10-14 19:46:23 +0200

    add basket store

--- web/app/components/ProductCard.vue
+++ web/app/components/ProductCard.vue
@@ -3,6 +3,7 @@ const props = defineProps<{
   drink: Drink
 }>()
 
+const basketStore = useBasketStore()
 const pb = usePocketbase()
 const img = computed(() => pb.files.getURL(props.drink, props.drink.photo, { thumb: '500x300' }))
 const price = computed(() => {
@@ -22,8 +23,14 @@ const price = computed(() => {
       <h3 class="card-title">{{ props.drink.drink }}</h3>
 
       <p>{{ price }}</p>
-      <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+      <div class="card-actions justify-center">
+        <div class="flex gap-2 items-center">
+          <button class="btn btn-primary" @click="basketStore.remove(props.drink.id)">-</button>
+          <p>
+            {{ basketStore.products[drink.id] }}
+          </p>
+          <button class="btn btn-primary" @click="basketStore.add(props.drink.id)">+</button>
+        </div>
       </div>
     </div>
   </div>
---
+++ web/app/stores/basket.ts
@@ -0,0 +1,33 @@
+export const useBasketStore = defineStore('basket', () => {
+  const products = ref<Record<string, number>>({})
+
+  const add = (id: string) => {
+    if (!products.value[id]) {
+      products.value[id] = 1
+    } else {
+      products.value[id] += 1
+    }
+  }
+
+  const remove = (id: string) => {
+    if (!products.value[id]) {
+      return
+    } else if (products.value[id] == 1) {
+      delete products.value[id]
+    } else {
+      products.value[id] -= 1
+    }
+  }
+
+  const clear = () => {
+    products.value = {};
+  }
+
+  return {
+    products,
+
+    add,
+    remove,
+    clear,
+  }
+})
